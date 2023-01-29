68a7188e6126aef0715d9a59341d82d161313c55
PIYUSH PANT <piyushpant15@gmail.com>
2023-01-29 20:34:11 +0530

    Search functionality added

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # PASSWORD MANAGER
- 
-There are 4 project files.
 
+There are 4 project files.
 
 - `main.py` is the main file of the project.
 
@@ -9,11 +8,10 @@ There are 4 project files.
 
 - `logo.png` is a logo for GUI of the project. Tkinter is used to create a canvas and load this image, hence you can replace with as per your choice to change the logo.
 
-- `data.txt` is the output generated file after a user saves a password
+- `data.json` is the output generated file after a user saves a password and uses it to search the website
 
-#### Execute the `main.py` to run your project. This project also showcase Validation and Verification in form submission using messageBox of tkinter. Along with that, user can create their password or use the GENERATE PASSWORD button to get a strong password.
+#### Execute the `main.py` to run your project. This project also showcase Validation and Verification in form submission using messageBox of tkinter. Along with that, user can create their password or use the GENERATE PASSWORD button to get a strong password. Additional feature of the project is the SEARCHING functionality.
 
-###### This mini project is developed to enhance learning and understanding of Python, TKinter, and advanced arguments. Anyone who wishes to explore and extend this project is welcome and I will happily add them as a collaborator. Thank you. 
-
+###### This mini project is developed to enhance learning and understanding of Python, TKinter, and advanced arguments. Anyone who wishes to explore and extend this project is welcome and I will happily add them as a collaborator. Thank you.
 
 > By Piyush Pant ( पियूष पंत द्वारा )
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+
+
 from tkinter import *
 from tkinter import messagebox  # This is not a class, so we need to import it again
-
+import json
 import pyperclip  # To automatically copy the password if it is generated or saved so that user can just take it and paste to portal
 
 
@@ -17,6 +19,54 @@ def genPassClicked():
     passwordInput.insert(0,newPass)
     pyperclip.copy(newPass) # To automatically copy the generated password
 
+# ---------------------------- SEARCH ------------------------------- #
+
+def Search():
+    
+    query = webInput.get()  # the website entered by the user
+    
+    try:
+        with open("Data.json","r") as f:
+            data = json.load(f)
+        
+        # for (key,value) in data.items():
+            
+        # if(key == query):
+        
+    except FileNotFoundError:
+        # if file is not there, means there is no data anyways so we will show error
+        messagebox.showwarning(title="No Data Found", message=f"There is no result for {query}")
+    
+        
+    else:
+        if query in data:
+            
+            foundData = data[query]
+            
+            
+            email = foundData["email"]
+            password = foundData["password"]
+            # now we will show user info box giving the password
+            
+            messagebox.showinfo(title= "Your Data", message=f"Website: {query}\nEmail: { email }\nPassword: {password}\n")
+            # webInput.delete(0,END)
+            
+
+        else:
+            messagebox.showwarning(title="No Data Found", message=f"There is no result for {query}")
+            # webInput.delete(0,END)
+        
+
+    finally:    
+        webInput.delete(0,END)
+                    
+                    
+                    
+                    
+                
+
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def Save():
@@ -24,6 +74,13 @@ def Save():
     email = emailInput.get()
     password = passwordInput.get()
     
+    # JSON data
+    newData = {
+        website : { 
+            "email":email,
+            "password":password
+        }
+    }
     
     # Validation
     if (len(website) == 0) or (len(email) == 0) or (len(password) == 0):
@@ -36,13 +93,40 @@ def Save():
         
         if isOK:
             
-            with open("Data.txt", "a") as f:
-                f.write(f"{website} | {email} | {password}\n")
+            try:
+            
+                with open("Data.json", "r") as f:
+                    
+                    # To store the data in JSON
+                    
+                    # First we will read the existing data
+                    existingData = json.load(f)
+            
+            except FileNotFoundError:
+                
+                with open("Data.json", "w") as f:
+                         
+                    json.dump(newData, f, indent=4)  
+                    
+        
+                    
+            else: # if the TRY block runs means there is already file and no need to create new, then we will just update
+                
+                # Secondly we will update the data
+                existingData.update(newData)    # now the new entry is added
+                    
+                with open("Data.json", "w") as f:
+                    
+                    # Third step is to write
+                    # JSON writing
+                    json.dump(existingData, f, indent=4)  # this method takes the data and name of file and many other arguments
+                    
+                    
+
+            finally: # no matter whether new file or old, we will have to clear the screen input 
                 # After getting data, then we will delete it from screen input
                 webInput.delete(0, END)
                 passwordInput.delete(0, END)
-                
-
 
 
 
@@ -79,9 +163,9 @@ password.grid(row=3, column =0, pady=5)
 
 
 
-webInput = Entry(width=40)
+webInput = Entry(width=22)
 webInput.focus()
-webInput.grid(row=1, column =1 , columnspan=2, pady=5)
+webInput.grid(row=1, column =1 , pady=5)
 
 emailInput = Entry(width=40)
 emailInput.grid(row=2, column =1 , columnspan=2, pady=5)
@@ -100,7 +184,8 @@ genPass.grid(row=3, column =2, pady=5)
 addButton = Button(text="Add Data", width=35, command= Save, bg="black", fg="white")
 addButton.grid(row=4, column =1, columnspan=2, pady=5)
 
-
+searchButton = Button(text="Search", width=10, command= Search, bg="crimson", fg="white")
+searchButton.grid(row=1, column =2, pady=5)
 
 win.mainloop()
 
